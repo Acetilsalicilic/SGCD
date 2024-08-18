6bccd268d750f069f7b201f59a8ab86330852a7c
Acetilsalicilic <1Uncanalmas@gmail.com>
2024-08-17 22:29:02 -0600

    Start of creation of interfaces and records of entities

---
+++ src/main/java/interfaces/DAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import Records.Sucursal;
+
+/**
+ *
+ * @author W10
+ */
+public interface DAO {
+    public Sucursal getSucursal(String nombre);
+}
