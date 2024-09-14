abfaba138596d08506552eb51e462e9104c4ec81
Vega <vegafernando881@gmail.com>
2024-09-13 22:53:39 -0600

    Deleted Other Constructors

--- src/main/java/Records/Usuario.java
+++ src/main/java/Records/Usuario.java
@@ -9,24 +9,5 @@ package Records;
  * @author W10
  */
 public record Usuario(Integer id_usuario, TipoUsuario tipoUsuario, String nombre_usuario, String contrasena) {
-    
-//    // Constructor Principal 
-//    public Usuario(Integer id_usuario, Integer id_tipo_usuario, String nombre_usuario, String contrasena, String desc_tipo) {
-//        this.id_usuario = id_usuario;
-//        this.id_tipo_usuario = id_tipo_usuario;
-//        this.nombre_usuario = nombre_usuario;
-//        this.contrasena = contrasena;
-//        this.desc_tipo = desc_tipo != null ? desc_tipo : "";
-//    }
-//    
-//    // Constructor Secundario 
-//    public Usuario(Integer id_usuario, Integer id_tipo_usuario, String nombre_usuario, String contrasena) {
-//        this(id_usuario, id_tipo_usuario, nombre_usuario, contrasena, "");
-//    }
-//    
-//    // Constructor Secundario 
-//    public Usuario(Integer id_usuario, Integer id_tipo_usuario) {
-//        this(id_usuario, id_tipo_usuario, "", "", "");
-//    }
-    
+        
 }
