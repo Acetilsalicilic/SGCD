118181f3b8a79cb6679b373620a8a02c3f9e0f17
Vega <vegafernando881@gmail.com>
2024-09-14 17:25:08 -0600

    Class Medico Completed. Methods and Test.

--- src/test/java/EntityDaoTests/MedicoDAOTest.java
+++ src/test/java/EntityDaoTests/MedicoDAOTest.java
@@ -5,6 +5,7 @@
 package EntityDaoTests;
 
 import DAO.EntityDAOPool;
+import Records.Medico;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,6 +37,18 @@ public class MedicoDAOTest {
         EntityDAOPool.close();
     }
     
+    // Create Medico Test
+    @Test
+    public void createMedico() {
+        int id_usuario = 10;
+        var usuario = EntityDAOPool.instance().getUsuarioDAO().getById(id_usuario);
+        var especialidad = EntityDAOPool.instance().getEspecialidadDAO().getTypeEspecialidad(1);
+        var rs = instance.getMedicoDAO().createMedico(new Medico(0, usuario, especialidad, "VegaDocID4", "Vega"));
+        System.out.println("ID Medico Created: " + rs);
+        assertNotNull(rs);
+    }
+    
+    // Get Medico By ID or All Test (Read)
     @Test
     public void getAllMedico() {
         var rs = instance.getMedicoDAO().getAll();
@@ -47,6 +60,47 @@ public class MedicoDAOTest {
     public void getMedicoById() {
         var rs = instance.getMedicoDAO().getById(1);
         System.out.println(rs);
+        assertNotNull(rs);
+    }
+    
+    // Find Medico By Name Test (Read)
+    @Test
+    public void getMedicoByName() {
+        String nombre_medico = "Vega";
+        var rs = instance.getMedicoDAO().getByNombre(nombre_medico);
+        System.out.println("Medico With Name " + nombre_medico + " Found: " + rs);
+        assertNotNull(rs);
+    }
+    
+    // Find Medico By Speciality Test (Read)
+    @Test
+    public void getMedicoBySpeciality() {
+        String speciality_desc = "Ortodoncia";
+        var rs = instance.getMedicoDAO().getBySpeciality(speciality_desc);
+        System.out.println("Medico(s) With Speciality " + speciality_desc + " Found: " + rs);
+        assertNotNull(rs);
+    }
+    
+    // Update Medico Test 
+    @Test 
+    public void updateMedico() {
+        int id_usuario = 10;
+        var usuario = EntityDAOPool.instance().getUsuarioDAO().getById(id_usuario);
+        int id_especialidad = 1;
+        var especialidad = EntityDAOPool.instance().getEspecialidadDAO().getTypeEspecialidad(id_especialidad);
+        int id_medico = 2;
+        var medico = EntityDAOPool.instance().getMedicoDAO().getById(id_medico);
+        var rs = instance.getMedicoDAO().updateMedico(new Medico(id_medico, usuario, especialidad, "Fernando", medico.apellidos_medico()));
+        System.out.println("Status Update: " + rs);
+        assertEquals(1, 1);
+    }
+    
+    // Delete Medico Test
+    @Test
+    public void deleteMedico() {
+        int id_medico = 4; 
+        var rs = instance.getMedicoDAO().deleteMedicoById(id_medico);
+        System.out.println("Medico With ID " + id_medico + " Deleted Status: " + rs);
         assertNotNull(rs);
     }
 }
